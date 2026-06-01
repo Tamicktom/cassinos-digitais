c8df6e4eff49e961f5c3d6210c34d48accffc653
Tamicktom <rickpokemon38@gmail.com>
2026-06-01 09:01:03 -0300

    ok

--- apps/web/src/slides/part-3/index.tsx
+++ apps/web/src/slides/part-3/index.tsx
@@ -1,12 +1,56 @@
+//* Libraries imports
+import { Stack } from "@revealjs/react"
+
 //* Components imports
 import { Slide } from "@/components/slide"
 
 export function Part3() {
   return (
     <>
+      <Stack>
       <Slide>
         <h2>UI/UX</h2>
       </Slide>
+
+      <Slide>
+        <h2 className="font-grand-casino text-5xl font-bold text-white pb-8">
+          Dark Patterns
+        </h2>
+        <p>
+          Escolhas de design que induzem usuários a fazer algo que talvez não fariam se a informação estivesse clara.
+        </p>
+      </Slide>
+
+      <Slide className="h-[65svh]">
+        <div className="w-full flex justify-center items-center">
+        <img
+          src="/assets/dragon.png"
+          alt="Dark Patterns 1"
+          className="w-full rounded-lg border"
+        />
+        </div>
+      </Slide>
+
+      <Slide className="h-[65svh]">
+        <div className="w-full flex justify-center items-center">
+        <img
+          src="/assets/spaceman.png"
+          alt="Dark Patterns 2"
+          className="w-full rounded-lg border"
+        />
+        </div>
+      </Slide>
+
+      <Slide className="h-[65svh]">
+        <div className="w-full flex justify-center items-center">
+        <img
+          src="/assets/candy-rush.png"
+          alt="Dark Patterns 3"
+          className="w-full rounded-lg border"
+        />
+        </div>
+      </Slide>
+      </Stack>
     </>
   )
 }
